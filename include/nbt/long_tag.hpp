3cc444e4cd748a131e83d03ad2847307a6d76c28
AwesomeCodingGuy <sonla@gmx.net>
2021-10-31 18:42:00 +0100

    Added LongTag including test.

--- include/nbt/long_tag.hpp
+++ include/nbt/long_tag.hpp
@@ -1,8 +1,42 @@
 #ifndef NBT_LONG_TAG_HPP
 #define NBT_LONG_TAG_HPP
 
+// nbt
+#include "nbt/abstract_tag.hpp"
+
+// STL
+#include <string>
+
 namespace nbt
 {
+
+class LongTag : public AbstractTag
+{
+public:
+    LongTag();
+    LongTag(const LongTag &other);
+    LongTag(LongTag &&other) noexcept;
+    LongTag(const std::string &name);
+    LongTag(int64_t value);
+    LongTag(const std::string &name, int64_t value);
+    virtual ~LongTag();
+
+    LongTag& operator=(const LongTag &other);
+    LongTag& operator=(LongTag &&other) noexcept;
+
+    constexpr virtual TagType getType() const override;
+
+    std::vector<unsigned char> getData(bool isListEntry) override;
+
+    int64_t getValue() const;
+    void setValue(int64_t value);
+
+protected:
+    virtual bool isEqual(const AbstractTag &other) const override;
+
+private:
+    int64_t m_value;
+};
 
 } // namespace nbt
 
