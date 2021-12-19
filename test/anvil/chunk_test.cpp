f9ec5601dc556844ee015c3f2e52527f2f320633
AwesomeCodingGuy <sonla@gmx.net>
2021-12-19 12:32:08 +0100

    Added unit test for getSubTagsByName.

--- test/anvil/chunk_test.cpp
+++ test/anvil/chunk_test.cpp
@@ -1,5 +1,6 @@
 #include "anvil/chunk.hpp"
 #include "nbt/tags/int_tag.hpp"
+#include "nbt/tags/float_tag.hpp"
 
 // gtest
 #include <gtest/gtest.h>
@@ -177,4 +178,60 @@ TEST(Chunk, setRootTag)
     EXPECT_EQ(chunk.getRootTag()->getType(), nbt::TagType::Compound);
     EXPECT_STREQ(chunk.getRootTag()->getName().c_str(), "setRootTag2");
     EXPECT_STREQ(chunk.getRootTag()->getValue()[0]->getName().c_str(), "IntTag");
+}
+
+TEST(Chunk, getSubTagsByName)
+{
+    // Init CompoundTag & Chunk
+    nbt::CompoundTag *root = new nbt::CompoundTag("getSubTagsByName");
+    nbt::IntTag *int1 = new nbt::IntTag("IntTag", 4);
+    root->pushBack(int1);
+
+    nbt::CompoundTag *subTag1 = new nbt::CompoundTag("sub1");
+    nbt::IntTag *int2 = new nbt::IntTag("IntTag", 5);
+    nbt::IntTag *int3 = new nbt::IntTag("IntTag", 6);
+    nbt::IntTag *int4 = new nbt::IntTag("IntTag", 7);
+    root->pushBack(int2);
+    root->pushBack(int3);
+    root->pushBack(int4);
+
+    nbt::CompoundTag *subTag2 = new nbt::CompoundTag("sub2");
+    nbt::FloatTag *float1 = new nbt::FloatTag("FloatTag", 55.f);
+    nbt::FloatTag *float2 = new nbt::FloatTag("FloatTag", 56.f);
+    nbt::FloatTag *float3 = new nbt::FloatTag("FloatTag", 57.f);
+    root->pushBack(float1);
+    root->pushBack(float2);
+    root->pushBack(float3);
+
+    root->pushBack(subTag1);
+    root->pushBack(subTag2);
+
+    anvil::Chunk chunk;
+    chunk.setRootTag(root);
+
+    // Test
+    std::vector<nbt::AbstractTag*> tags1 = chunk.getSubTagsByName("getSubTagsByName");
+    EXPECT_EQ(1, tags1.size());
+    EXPECT_EQ(tags1[0], root);
+
+    std::vector<nbt::AbstractTag*> tags2 = chunk.getSubTagsByName("sub1");
+    EXPECT_EQ(1, tags2.size());
+    EXPECT_EQ(tags2[0], subTag1);
+
+    std::vector<nbt::AbstractTag*> tags3 = chunk.getSubTagsByName("sub2");
+    EXPECT_EQ(1, tags3.size());
+    EXPECT_EQ(tags3[0], subTag2);
+
+    std::vector<nbt::AbstractTag*> tags4 = chunk.getSubTagsByName("IntTag");
+    EXPECT_EQ(4, tags4.size());
+    EXPECT_EQ(tags4[0], int1);
+    EXPECT_EQ(tags4[1], int2);
+    EXPECT_EQ(tags4[2], int3);
+    EXPECT_EQ(tags4[3], int4);
+
+    std::vector<nbt::AbstractTag*> tags5 = chunk.getSubTagsByName("FloatTag");
+    EXPECT_EQ(3, tags5.size());
+    EXPECT_EQ(tags5[0], float1);
+    EXPECT_EQ(tags5[1], float2);
+    EXPECT_EQ(tags5[2], float3);
 }
