2413d251bd5fdf292566dc35f33725ecbc05f38d
AwesomeCodingGuy <sonla@gmx.net>
2022-01-12 00:14:52 +0100

    Added Linux specific bit_cast and bswap function, since GCC 10 does not have C++20 bit_cast yet.

--- include/AwesomeMC/util/byte_swap.hpp
+++ include/AwesomeMC/util/byte_swap.hpp
@@ -4,6 +4,7 @@
 // STL
 #include <cstdint>
 #include <bit>
+#include <cstring>
 #include <type_traits>
 #include <concepts>
 
@@ -37,6 +38,18 @@ inline auto bswap(std::uint16_t v) noexcept {
     return __builtin_bswap16(v);
 }
 
+// g++-10 does not yet have bit_cast available
+template<class To, class From>
+std::enable_if_t<sizeof(To) == sizeof(From) && std::is_trivially_copyable_v<To> && std::is_trivially_copyable_v<From>, To>
+bit_cast(const From &src) noexcept
+{
+    static_assert(std::is_trivially_constructible_v<To>,
+                  "This implementation additionally requires destination type to be trivially constructible");
+    To dst;
+    std::memcpy(&dst, &src, sizeof(To));
+    return dst;
+}
+
 #endif
 
 } // namespace detail
@@ -49,11 +62,19 @@ inline auto bswap(std::integral auto value) noexcept {
     }
 }
 
+#if defined(_MSC_VER)
 inline auto bswap(std::floating_point auto value) noexcept {
     std::conditional_t<sizeof(decltype(value)) <= sizeof(int32_t), int32_t, int64_t> out{
         std::bit_cast<decltype(out), decltype(value)>(value)};
     return std::bit_cast<decltype(value), decltype(out)>(detail::bswap(static_cast<std::make_unsigned_t<decltype(out)>>(out)));
 }
+#elif defined(__GNUC__)
+inline auto bswap(std::floating_point auto value) noexcept {
+    std::conditional_t<sizeof(decltype(value)) <= sizeof(int32_t), int32_t, int64_t> out{
+        detail::bit_cast<decltype(out), decltype(value)>(value)};
+    return detail::bit_cast<decltype(value), decltype(out)>(detail::bswap(static_cast<std::make_unsigned_t<decltype(out)>>(out)));
+}
+#endif
 
 } // namespace util
 
